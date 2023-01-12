cc67d087287732b42aee511f757ba978329dfab1
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-01-12 20:52:45 +0900

    지우개, 이미지 추가

--- javascript-project/app.js
+++ javascript-project/app.js
@@ -1,10 +1,15 @@
+const fileInput = document.getElementById("file");
+const eraseBtn = document.getElementById("eraser-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
 const lineWidth =document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
-
 // context = 붓
 const ctx = canvas.getContext("2d")
+const CANVAS_WIDTH =400;
+const CANVAS_HEIGHT = 400;
 canvas.width = 400;
 canvas.height = 400;
 
@@ -12,8 +17,9 @@ ctx.lineWidth =lineWidth.value;
 
 ctx.moveTo(200, 200);
 
-
 let isPainting = false;
+let isFilling = false;
+
 function onMove(event) {
     if(isPainting){
         ctx.lineTo(event.offsetX, event.offsetY);
@@ -41,14 +47,54 @@ function onColorChange(event) {
 }
 function onColorClick(event) {
     // console.dir(event.target.dataset.color);
-    ctx.strokeStyle = event.target.dataset.color;
-    ctx.fillStyle = event.target.dataset.color;
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
 }
+function onModeClick() {
+    if(isFilling) {
+        isFilling = false;
+        modeBtn.innerText="Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText="Draw";
+    }
+}
+function onCanvasClick() {
+    if(isFilling){
+        ctx.fillRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
+function onDestroyClick() {
+    ctx.fillStyle ="white";
+    Rect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+}
+function onEraseClick() {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText="Fill";
+}
+function onFileChange(event){
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src = url;
+    image.onload = function() {
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        fileInput.value = null;
+    };
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave",  cancelPainting);
-
+canvas.addEventListener("click", onCanvasClick);
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraseBtn.addEventListener("click", onEraseClick);
+fileInput.addEventListener("change", onFileChange);
